43d2ecd388d11a51255f97c8dac7f6206b6f485e
chetryJyoti <jyotichetry087@gmail.com>
2023-02-26 13:51:01 +0530

    added errHander and cors,cookie-parser

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3500',
+    'https://www.chetry.com',
+    'https://chetry.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const {logger} = require('./middleware/logger.js')
+const {logger} = require('./middleware/logger')
+const errorHandler =require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500;
 
 app.use(logger)
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use("/", express.static(path.join(__dirname, 'public')));
 
@@ -22,6 +30,8 @@ app.all('*',(req,res)=>{
         res.type('txt').send('404 not found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
