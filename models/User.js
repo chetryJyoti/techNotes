090d4cda9c9646f1cf204e5dce74f9e0ecc6cea3
chetryJyoti <jyotichetry087@gmail.com>
2023-02-28 00:49:27 +0530

    done with User and Note Schema and added mongoose-sequence auto increment feature

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  //to be continued
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,38 +1,54 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
-const {logger} = require('./middleware/logger')
-const errorHandler =require('./middleware/errorHandler')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConection");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
-app.use(logger)
+console.log(process.env.NODE_ENV);
 
-app.use(cors(corsOptions))
+connectDB();
+app.use(logger);
 
-app.use(express.json())
+app.use(cors(corsOptions));
 
-app.use(cookieParser())
+app.use(express.json());
 
-app.use("/", express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
-app.use('/',require('./routes/root'))
+app.use("/", express.static(path.join(__dirname, "public")));
 
-app.all('*',(req,res)=>{
-    res.status(404)
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname,'views','404.html'))
-    }else if(req.accepts('json')){
-        res.json({message:'404 not found'})
-    }else{
-        res.type('txt').send('404 not found')
-    }
-})
+app.use("/", require("./routes/root"));
 
-app.use(errorHandler)
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("CONNECTED TO MongoDB");
+  app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}:${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
