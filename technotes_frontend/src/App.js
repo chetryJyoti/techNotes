b2408c14be9da146245435a5fc47cc36874f38d8
chetryJyoti <jyotichetry087@gmail.com>
2023-03-19 14:15:53 +0530

    added edit and create new routing

--- technotes_frontend/src/App.js
+++ technotes_frontend/src/App.js
@@ -6,6 +6,10 @@ import Public from "./components/Public";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features//notes/NewNote";
 
 function App() {
   return (
@@ -13,15 +17,23 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
+
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
+          </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
+        {/*dash route ends*/}
       </Route>
     </Routes>
   );
