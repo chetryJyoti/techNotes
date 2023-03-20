2cf048fa5c07d813f0240da3ff78caf9f4f857db
chetryJyoti <jyotichetry087@gmail.com>
2023-03-20 20:33:09 +0530

    done with the edit user form: update and dlt user

--- technotes_frontend/src/features/users/EditUser.js
+++ technotes_frontend/src/features/users/EditUser.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersApiSlice";
+import EditUserForm from "./EditUserForm";
 
 const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
-}
+  const { id } = useParams();
+  // console.log("userId:", id);
+  const user = useSelector((state) => selectUserById(state, id));
+  // console.log("user:", user);
+  const content = user ? <EditUserForm user={user} /> : <p>Loading user...</p>;
+  return content;
+};
 
-export default EditUser
+export default EditUser;
