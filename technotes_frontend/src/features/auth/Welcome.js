691f5bfcfab035cd79f470c8c5df80c30f75de70
chetryJyoti <jyotichetry087@gmail.com>
2023-03-20 21:18:16 +0530

    added other pages links

--- technotes_frontend/src/features/auth/Welcome.js
+++ technotes_frontend/src/features/auth/Welcome.js
@@ -11,9 +11,8 @@ function Welcome() {
     minute: "numeric",
     hour12: true,
   };
-  const currentDate = new Date().toLocaleDateString('en-US', options);
-  
-  
+  const currentDate = new Date().toLocaleDateString("en-US", options);
+
   const content = (
     <section className="welcome">
       <p>{currentDate}</p>
@@ -21,11 +20,19 @@ function Welcome() {
       <h1>Welcome!</h1>
 
       <p>
-        <Link to="/dash/notes">View techNotes</Link>
+        <Link to="/dash/notes">View AllNotes</Link>
       </p>
 
       <p>
-        <Link to="/dash/users">View User Settings</Link>
+        <Link to="/dash/notes/new">Add NewNote</Link>
+      </p>
+
+      <p>
+        <Link to="/dash/users/new">Add NewUser</Link>
+      </p>
+
+      <p>
+        <Link to="/dash/users"> User Settings</Link>
       </p>
     </section>
   );
