9e1665035845123d397d198969a537b9f730ad06
chetryJyoti <jyotichetry087@gmail.com>
2023-03-20 20:32:32 +0530

    keepUnusedDataFor removed

--- technotes_frontend/src/features/notes/notesApiSlice.js
+++ technotes_frontend/src/features/notes/notesApiSlice.js
@@ -17,7 +17,7 @@ export const notesApiSlice = apiSlice.injectEndpoints({
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
       },
-      keepUnusedDataFor: 5,
+      // keepUnusedDataFor: 5,
       transformResponse: (responseData) => {
         const loadedNotes = responseData.map((user) => {
           user.id = user._id;
