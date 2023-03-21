49410fc5e057fa90bfa693c7d350e0d0f1770ae4
chetryJyoti <jyotichetry087@gmail.com>
2023-03-21 14:43:32 +0530

    ui and logic to edit the existing note impl

--- technotes_frontend/src/features/notes/EditNote.js
+++ technotes_frontend/src/features/notes/EditNote.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectNoteById } from "../notes/notesApiSlice";
+import { selectAllUsers } from "../users/usersApiSlice";
+import EditNoteForm from "./EditNoteForm";
 const EditNote = () => {
-  return (
-    <div>EditNote</div>
-  )
-}
+  const { id } = useParams();
+  const note = useSelector((state) => selectNoteById(state, id));
+  const users = useSelector(selectAllUsers);
+  const content =
+    note && users ? (
+      <EditNoteForm note={note} users={users} />
+    ) : (
+      <p>Loading....</p>
+    );
+  return content;
+};
 
-export default EditNote
+export default EditNote;
