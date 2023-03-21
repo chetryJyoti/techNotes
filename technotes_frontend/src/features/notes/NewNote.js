02e91a2cc502394670dfef4247bae2b8d45c03e6
chetryJyoti <jyotichetry087@gmail.com>
2023-03-21 14:44:02 +0530

    ui and logic to create new note impl

--- technotes_frontend/src/features/notes/NewNote.js
+++ technotes_frontend/src/features/notes/NewNote.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {useSelector} from "react-redux"
+import {selectAllUsers} from "../users/usersApiSlice"
+import NewNoteForm from './NewNoteForm'
 
+//here we are getting all users in the note form because we want to assign the notes to a user
 const NewNote = () => {
-  return (
-    <div>NewNote</div>
-  )
+  const users = useSelector(selectAllUsers)
+  const content = users?<NewNoteForm users={users}/>:<p>Loading ....</p>
+    
+  return content
 }
 
-export default NewNote
+export default NewNote 
