0dc7a16e3dad735e1bec89120e78b553c97f6d51
chetryJyoti <jyotichetry087@gmail.com>
2023-03-22 23:50:57 +0530

    added:verifying the routes with valid jwt token and  authenticating the user to access the end points

--- technotes_backend/routes/noteRoutes.js
+++ technotes_backend/routes/noteRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const notesController = require('../controllers/notesController')
+const notesController = require("../controllers/notesController");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.route('/')
-           .get(notesController.getAllNotes)
-           .post(notesController.createNote)
-           .patch(notesController.updateNote)
-           .delete(notesController.deleteNote)
 
-module.exports = router
+router.use(verifyJWT);
+router
+  .route("/")
+  .get(notesController.getAllNotes)
+  .post(notesController.createNote)
+  .patch(notesController.updateNote)
+  .delete(notesController.deleteNote);
+
+module.exports = router;
--- technotes_backend/routes/userRoutes.js
+++ technotes_backend/routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const usersController = require('../controllers/usersController')
 
+const verifyJWT = require("../middleware/verifyJWT")
+
+router.use(verifyJWT)
 router.route('/')
            .get(usersController.getAllUsers)
            .post(usersController.createNewUser)
