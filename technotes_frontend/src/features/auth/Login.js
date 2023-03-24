3b3347a5d60ac9f3283645fa2ce3ec2e6d1cbe65
chetryJyoti <jyotichetry087@gmail.com>
2023-03-24 23:11:40 +0530

    login form and error-handling code done

--- technotes_frontend/src/features/auth/Login.js
+++ technotes_frontend/src/features/auth/Login.js
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { setCredentials } from "../auth/authSlice";
+import { useLoginMutation } from "./authApiSlice";
+import { useDispatch } from "react-redux";
+import { ThreeDots } from "react-loader-spinner";
 
-function Login() {
-  return (
-    <div>Login comming soon</div>
-  )
-}
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
-export default Login
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  //to remove the error msg if earlier there was any error msg as soon as use starts typing username or password
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  if (isLoading)
+    return (
+      <div className="spinner">
+        <ThreeDots
+          height="50"
+          width="100"
+          radius="19"
+          color="lightblue"
+          ariaLabel="three-dots-loading"
+          visible={true}
+        />
+      </div>
+    );
+
+  const handleUserInput = (e) => setUsername(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("submited");
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+      navigate("/dash");
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg("No server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing username or password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(err.data?.message);
+      }
+      console.log("error:", err);
+      errRef.current.focus();
+    }
+  };
+
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className="login">
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+          <button className="form__submit-button">Sign In</button>
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  );
+
+  return content;
+};
+
+export default Login;
