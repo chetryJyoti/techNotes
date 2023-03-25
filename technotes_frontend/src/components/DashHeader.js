738fbbf54601621dc74c622e2ab9ca044ec4a0d7
chetryJyoti <jyotichetry087@gmail.com>
2023-03-25 23:19:11 +0530

    added:Logout feature on various routes

--- technotes_frontend/src/components/DashHeader.js
+++ technotes_frontend/src/components/DashHeader.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
-function DashHeader() {
+import React, { useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useSendLogoutMutation } from "../features/auth/authApiSlice";
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
+
+const DashHeader = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
+
+  useEffect(() => {
+    if (isSuccess) navigate("/");
+  }, [isSuccess, navigate]);
+
+  if (isLoading) return <p>Logging Out...</p>;
+
+  if (isError) return <p>Error: {error.data?.message}</p>;
+
+  let dashClass = null;
+  if (
+    !DASH_REGEX.test(pathname) &&
+    !NOTES_REGEX.test(pathname) &&
+    !USERS_REGEX.test(pathname)
+  ) {
+    dashClass = "dash-header__container--small";
+  }
+  const logoutButton = (
+    <button className="icon-button" title="Logout" onClick={sendLogout}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  );
+
   const content = (
     <header className="dash-header">
-      <div className="dash-header__container">
+      <div className={`dash-header__container ${dashClass}`}>
         <Link to="/dash">
           <h1 className="dash-header__title">Tech notes</h1>
         </Link>
-
-        <nav className="dash-header__nav"></nav>
+        <nav className="dash-header__nav">{logoutButton}</nav>
       </div>
     </header>
-  )
+  );
   return content;
-}
+};
 
 export default DashHeader;
