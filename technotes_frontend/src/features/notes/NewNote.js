0bd0cd4023fa1d36f19e870d9bfe9dc245c02af9
chetryJyoti <jyotichetry087@gmail.com>
2023-03-25 23:20:24 +0530

    disabled users data info if no the array response length

--- technotes_frontend/src/features/notes/NewNote.js
+++ technotes_frontend/src/features/notes/NewNote.js
@@ -2,25 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersApiSlice";
 import NewNoteForm from "./NewNoteForm";
-import { ThreeDots } from "react-loader-spinner";
+
 //here we are getting all users in the note form because we want to assign the notes to a user
 const NewNote = () => {
   const users = useSelector(selectAllUsers);
-  const content = users ? (
+  if(!users?.length) return <p>Not Available</p>
+  const content =  (
     <NewNoteForm users={users} />
-  ) : (
-    <div className="spinner">
-      <ThreeDots
-        height="50"
-        width="100"
-        radius="19"
-        color="lightblue"
-        ariaLabel="three-dots-loading"
-        visible={true}
-      />
-    </div>
-  );
-
+  ) 
   return content;
 };
 
