e9b6a9e5c539a739787b719c75e360ef98610760
chetryJyoti <jyotichetry087@gmail.com>
2023-03-29 01:20:54 +0530

    added refresh code to automatically refresh the access token if it has expired and retry the request with new token

--- technotes_frontend/src/app/api/apiSlice.js
+++ technotes_frontend/src/app/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import {setCredentials} from "../../features/auth/authSlice"
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:3500",
   credentials: "include",
@@ -12,8 +12,43 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  console.log("args:",args) // request url, method, body
+  console.log("api:",args) // signal, dispatch, getState()
+  console.log("extraOptions:",extraOptions) //custom like {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions)
+  
+  console.log("result",result);
+  // If you want, handle other status codes, too
+  if (result?.error?.status === 403) {
+      console.log('sending refresh token')
+
+      // send refresh token to get new access token 
+      const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
+      console.log("refreshResult:",refreshResult);
+
+      if (refreshResult?.data) {
+
+          // store the new token 
+          api.dispatch(setCredentials({ ...refreshResult.data }))
+
+          // retry original query with new access token
+          result = await baseQuery(args, api, extraOptions)
+      } else {
+
+          if (refreshResult?.error?.status === 403) {
+              refreshResult.error.data.message = "Your login has expired. "
+          }
+          return refreshResult
+      }
+  }
+
+  return result
+}
+
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery:baseQueryWithReauth,
   tagTypes: ["Note", "User"],
   endpoints: (builder) => ({}),
 });
