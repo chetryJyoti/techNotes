481201e4410709d24a012b6016617faa3bfd2a5f
chetryJyoti <jyotichetry087@gmail.com>
2023-04-01 14:57:56 +0530

    added functionality for persisting the user to stay logged in even if the user refreshes the page

--- technotes_frontend/src/features/auth/authApiSlice.js
+++ technotes_frontend/src/features/auth/authApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from "../../app/api/apiSlice";
-import { logOut } from "../auth/authSlice";
+import { logOut,setCredentials } from "../auth/authSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -34,6 +34,16 @@ export const authApiSlice = apiSlice.injectEndpoints({
         url: "/auth/refresh",
         method: "GET",
       }),
+      async onQueryStarted(arg,{dispatch,queryFulfilled}){
+        try{
+          const {data} = await queryFulfilled
+          console.log(data);
+          const {accessToken} = data
+          dispatch(setCredentials({accessToken}))
+        }catch(err){
+          console.log("error:",err);
+        }
+      }
     }),
   }),
 });
--- technotes_frontend/src/hooks/usePersist.js
+++ technotes_frontend/src/hooks/usePersist.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// The purpose of this hook is to persist a boolean value in the browser's localStorage.
 const usePersist = () => {
   const [persist, setPersist] = useState(
     JSON.parse(localStorage.getItem("persist")) || false
