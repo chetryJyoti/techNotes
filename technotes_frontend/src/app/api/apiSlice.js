9fba409b2e528e48b0ed744f1ed699b3bb8c3c60
chetryJyoti <jyotichetry087@gmail.com>
2023-04-09 21:39:00 +0530

    backend prod link added

--- technotes_frontend/src/app/api/apiSlice.js
+++ technotes_frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {setCredentials} from "../../features/auth/authSlice"
 const baseQuery = fetchBaseQuery({
   //need to change this in production
-  baseUrl: "http://localhost:3500",
+  baseUrl: "https://technotesapi-bj5z.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
